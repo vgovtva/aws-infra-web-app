58d5fd2141ecf14cd9d5e9338a0e9805b2ee3b62
Vladislav Govtva <vlad.govtva@gmail.com>
2021-08-14 12:48:27 +0000

    Add S3 and DDB endpoints to vpc

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -13,12 +13,58 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-egress-gw"
+  }
+}
+
+
 resource "aws_egress_only_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "main-egress-gw"
   }
+}
+
+data "aws_vpc_endpoint_service" "s3" {
+  service      = "s3"
+  service_type = "Gateway"
+}
+
+data "aws_vpc_endpoint_service" "dynamodb" {
+  service      = "dynamodb"
+  service_type = "Gateway"
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.main.id
+  service_name = data.aws_vpc_endpoint_service.s3.service_name
+}
+
+resource "aws_vpc_endpoint" "dynamodb" {
+  vpc_id       = aws_vpc.main.id
+  service_name = data.aws_vpc_endpoint_service.dynamodb.service_name
+}
+
+resource "aws_default_route_table" "main" {
+  default_route_table_id = aws_vpc.main.default_route_table_id
+  tags = {
+    Name = "main-default-table"
+  }
+}
+
+resource "aws_vpc_endpoint_route_table_association" "s3" {
+  route_table_id  = aws_default_route_table.main.id
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+}
+
+resource "aws_vpc_endpoint_route_table_association" "dynamodb" {
+  route_table_id  = aws_default_route_table.main.id
+  vpc_endpoint_id = aws_vpc_endpoint.dynamodb.id
 }
 
 # Needed for sharing vpc_id with other resources in the module
